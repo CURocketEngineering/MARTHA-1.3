f726934083c09fef5da31e27e553fd56e14cae8e
Elan456 <telan4892@gmail.com>
2024-10-06 23:09:32 -0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+
+#include "bmp_spi.h"
+#include "Adafruit_LSM6DSOX.h"
+#include "Adafruit_LIS3MDL.h"
+
+#include "data_handling/SensorDataHandler.h"
+#include "data_handling/DataSaverSPI.h"
+#include "data_handling/DataNames.h"
+
+Adafruit_LSM6DSOX sox;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  Serial.println("Setting ACL and Gyro ranges and data rates...");
+  sox.setAccelRange(LSM6DS_ACCEL_RANGE_16_G);
+  sox.setGyroRange(LSM6DS_GYRO_RANGE_2000_DPS );
+
+  sox.setAccelDataRate(LSM6DS_RATE_104_HZ);
+  sox.setGyroDataRate(LSM6DS_RATE_104_HZ);
+
+  // If the range is not set correctly, then print a message
+  if (sox.getAccelRange() != LSM6DS_ACCEL_RANGE_16_G) {
+    Serial.println("Failed to set ACL range");
+  }
+  if (sox.getGyroRange() != LSM6DS_GYRO_RANGE_2000_DPS) {
+    Serial.println("Failed to set Gyro range");
+  }
+  if (sox.getAccelDataRate() != LSM6DS_RATE_104_HZ) {
+    Serial.println("Failed to set ACL data rate");
+  }
+  if (sox.getGyroDataRate() != LSM6DS_RATE_104_HZ) {
+    Serial.println("Failed to set Gyro data rate");
+  }
+}
+
+void loop() {
+
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+  sox.getEvent(&accel, &gyro, &temp);
+  
+  Serial.print("X: "); Serial.print(accel.acceleration.x); Serial.print(" m/s^2\t");
+  Serial.print("Y: "); Serial.print(accel.acceleration.y); Serial.print(" m/s^2\t");
+  Serial.print("Z: "); Serial.print(accel.acceleration.z); Serial.println(" m/s^2");
+}
