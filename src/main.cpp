ac5a99115930a0b6a100c70ae0b2bbc08530d3fc
Mason Vari <mvari@clemson.edu>
2024-10-29 22:05:34 -0400

    Adding flash driver to main

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,30 @@
 #include "bmp_spi.h"
 #include "Adafruit_LSM6DSOX.h"
 #include "Adafruit_LIS3MDL.h"
+#include "FlashDriver.h"
 
 #include "data_handling/SensorDataHandler.h"
 #include "data_handling/DataSaverSPI.h"
 #include "data_handling/DataNames.h"
 
 Adafruit_LSM6DSOX sox;
+FlashDriver flash;
 
 void setup() {
+
+
   // put your setup code here, to run once:
   Serial.begin(115200);
+  pinMode(PA4, OUTPUT);
+  FlashStatus result = flash.initFlash();
+  
+  if(result == FLASH_SUCCESS){
+    Serial.println("Flash Initialized!");
+  } else if(result == FLASH_INVALID){
+    Serial.println("SPI Wasn't Initalized!");
+  }else{
+    Serial.println("FLASH Wasn't Initalized!");
+  }
 
   Serial.println("Setting ACL and Gyro ranges and data rates...");
   sox.setAccelRange(LSM6DS_ACCEL_RANGE_16_G);
@@ -42,8 +56,14 @@ void loop() {
   sensors_event_t gyro;
   sensors_event_t temp;
   sox.getEvent(&accel, &gyro, &temp);
+
   
   Serial.print("X: "); Serial.print(accel.acceleration.x); Serial.print(" m/s^2\t");
   Serial.print("Y: "); Serial.print(accel.acceleration.y); Serial.print(" m/s^2\t");
   Serial.print("Z: "); Serial.print(accel.acceleration.z); Serial.println(" m/s^2");
+
+
+
 }
+
+
