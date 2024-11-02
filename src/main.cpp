ecd72d9628bd6bcc43500c2850201bba1f8cb2f3
Mason Vari <mvari@clemson.edu>
2024-11-02 13:15:57 -0400

    Getting sensors working

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,47 @@
 #include <Arduino.h>
 
-#include "bmp_spi.h"
 #include "Adafruit_LSM6DSOX.h"
 #include "Adafruit_LIS3MDL.h"
 #include "FlashDriver.h"
+#include "bmp_spi.h"
+#include "bmp3.h"
 
 #include "data_handling/SensorDataHandler.h"
 #include "data_handling/DataSaverSPI.h"
 #include "data_handling/DataNames.h"
 
+
+
 Adafruit_LSM6DSOX sox;
-FlashDriver flash;
+Adafruit_LIS3MDL  mag;
+FlashDriver       flash;
+BMP3_SPI          baro(PA1, PB5, PB4, PB3);
 
 void setup() {
 
 
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(PA4, OUTPUT);
-  FlashStatus result = flash.initFlash();
-  
-  if(result == FLASH_SUCCESS){
+
+  pinMode(PA4, OUTPUT); //FLASH
+
+
+  FlashStatus resultFlash = flash.initFlash();
+  if(resultFlash == FLASH_SUCCESS){
     Serial.println("Flash Initialized!");
-  } else if(result == FLASH_INVALID){
-    Serial.println("SPI Wasn't Initalized!");
   }else{
     Serial.println("FLASH Wasn't Initalized!");
   }
+
+
+
+  Serial.println("Setting up accelerometer and gyroscope...");
+  while (!sox.begin_SPI(PA0, PB3, PB4,
+                 PB5)){
+    Serial.println("Could not find sensor. Check wiring.");
+    delay(10);
+  }
+
 
   Serial.println("Setting ACL and Gyro ranges and data rates...");
   sox.setAccelRange(LSM6DS_ACCEL_RANGE_16_G);
@@ -48,6 +63,36 @@ void setup() {
   if (sox.getGyroDataRate() != LSM6DS_RATE_104_HZ) {
     Serial.println("Failed to set Gyro data rate");
   }
+
+  // Setup for the magnetometer
+  Serial.println("Setting up magnetometer...");
+  while (!mag.begin_SPI(PA3, PB3, PB4,
+                 PB5)) {
+    Serial.println("Could not find sensor. Check wiring.");
+    delay(10);
+  }
+  mag.setDataRate(LIS3MDL_DATARATE_155_HZ);
+  mag.setRange(LIS3MDL_RANGE_4_GAUSS);
+  mag.setOperationMode(LIS3MDL_SINGLEMODE);
+  mag.setPerformanceMode(LIS3MDL_MEDIUMMODE);
+
+  mag.setIntThreshold(500);
+  mag.configInterrupt(false, false, true, // enable z axis
+                          true, // polarity
+                          false, // don't latch
+                          true); // enabled!
+
+  if (mag.getDataRate() != LIS3MDL_DATARATE_155_HZ) {
+    Serial.println("Failed to set Mag data rate");
+  }
+
+    // Setup for the magnetometer
+  Serial.println("Setting up barometer...");
+  if(!baro.init()) {
+    Serial.println("Could not find sensor. Check wiring.");
+    delay(10);
+  }
+
 }
 
 void loop() {
@@ -55,15 +100,37 @@ void loop() {
   sensors_event_t accel;
   sensors_event_t gyro;
   sensors_event_t temp;
+  float magx;
+  float magy;
+  float magz;
+  bmp_data sensorData;
+  baro.getSensorData(&sensorData, true);
   sox.getEvent(&accel, &gyro, &temp);
+  mag.readMagneticField(magx, magy, magz);
 
-  
+  Serial.print("BMP390 DATA:\r\n");
+  Serial.print("Pressure: "); Serial.print(sensorData.pressure); Serial.print(" Pa\t");
+  Serial.print("Altitude: "); Serial.print(sensorData.altitude); Serial.print(" m\t");
+  Serial.print("Temperature: "); Serial.print(sensorData.temperature); Serial.println(" C\n");
+
+  Serial.print("LSM6DSOX DATA:\r\n");
   Serial.print("X: "); Serial.print(accel.acceleration.x); Serial.print(" m/s^2\t");
   Serial.print("Y: "); Serial.print(accel.acceleration.y); Serial.print(" m/s^2\t");
   Serial.print("Z: "); Serial.print(accel.acceleration.z); Serial.println(" m/s^2");
+  Serial.print("X: "); Serial.print(gyro.gyro.x); Serial.print(" d/s\t");
+  Serial.print("Y: "); Serial.print(gyro.gyro.y); Serial.print(" d/s\t");
+  Serial.print("Z: "); Serial.print(gyro.gyro.z); Serial.println(" d/s\n");
+
+  Serial.print("LIS3MDL DATA:\r\n");
+  Serial.print("X: "); Serial.print(magx); Serial.print(" µT\t");
+  Serial.print("Y: "); Serial.print(magy); Serial.print(" µT\t");
+  Serial.print("Z: "); Serial.print(magz); Serial.println(" µT\n");
 
 
+  delay(1000);
 
 }
+
+
 
 
