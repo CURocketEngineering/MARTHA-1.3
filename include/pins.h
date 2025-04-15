959882c5c5eff312523af33b8bb784f8745166a7
Elan456 <telan4892@gmail.com>
2025-04-15 00:42:23 -0400

    4-13 aa launch

--- include/pins.h
+++ include/pins.h
@@ -13,6 +13,7 @@
 #define SENSOR_LIS_CS PA2
 #define FLASH_CS PB1
 #define DEBUG_LED PA9
+#define BATTERY_VOLTAGE_ADC PB11
 
 // For the Adafruit SPI Flash
 #define EXTERNAL_FLASH_USE_CS PB1
@@ -30,7 +31,6 @@
 #define SENSOR_LIS_CS PA3
 #define FLASH_CS PB1
 #define DEBUG_LED PA9
-#define BATTERY_VOLTAGE PB11
 
 #define EXTERNAL_FLASH_USE_CS PB1
 #define EXTERNAL_FLASH_USE_SPI SPI
